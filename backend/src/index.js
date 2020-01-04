bd7322133ebfa583300f94a1a4cce6f85d810bb4
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-01-04 17:50:59 -0300

    feat: add mongoose
    fix: #4

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/devradar", {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
