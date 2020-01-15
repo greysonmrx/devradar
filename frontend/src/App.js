dfa5ed2e3d89b66a8e3722d248cc294909599265
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-01-15 17:34:07 -0300

    feat(frontend): create DevForm component
    fix: #17

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,36 +4,18 @@ import "./global.css";
 import "./Sidebar.css";
 import "./App.css";
 
+import DevForm from "./components/DevForm";
+
 function App() {
+  async function handleAddDev(data) {
+    console.log(data);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main></main>
     </div>
