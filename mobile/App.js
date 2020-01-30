71b1e73ce45cd55162889f00de7683872b783c46
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-01-30 18:47:34 -0300

    feat(mobile): create main page
    fix: #24

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, StatusBar } from "react-native";
 
+import Main from "./src/pages/Main";
+
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
-      <View></View>
+      <Main />
     </>
   );
 }
