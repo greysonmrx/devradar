570be380f93223e970efdf4eb340b5758c3576dc
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-01-31 18:52:03 -0300

    feat(mobile): create function to load initial position
    fix: #29

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, TextInput, TouchableOpacity } from "react-native";
 import MapView from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 import { MaterialIcons } from "@expo/vector-icons";
 
 function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  function handleRegionChange(region) {
+    setCurrentRegion(region);
+  }
+
   return (
     <>
-      <MapView style={styles.map} />
+      <MapView
+        onRegionChange={handleRegionChange}
+        initialRegion={currentRegion}
+        style={styles.map}
+      />
       <View style={styles.searchForm}>
         <TextInput
           style={styles.searchInput}
