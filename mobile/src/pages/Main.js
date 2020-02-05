a18b5885557ba4b78f68098d2829d58c96244b0c
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-02-05 19:20:56 -0300

    feat(mobile): create function to load Dev's
    fix: #31

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, TextInput, TouchableOpacity } from "react-native";
-import MapView from "react-native-maps";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
 } from "expo-location";
 import { MaterialIcons } from "@expo/vector-icons";
 
+import api from "../services/api";
+
 function Main() {
+  const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
+  const [techs, setTechs] = useState("");
 
   useEffect(() => {
     async function loadInitialPosition() {
@@ -37,13 +48,51 @@ function Main() {
     setCurrentRegion(region);
   }
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get("/search", {
+      params: {
+        latitude,
+        longitude,
+        techs,
+      },
+    });
+
+    setDevs(response.data);
+  }
+
+  if (!currentRegion) {
+    return null;
+  }
+
   return (
     <>
       <MapView
         onRegionChange={handleRegionChange}
         initialRegion={currentRegion}
         style={styles.map}
-      />
+      >
+        {devs.map((dev) => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              longitude: dev.location.coordinates[0],
+              latitude: dev.location.coordinates[1],
+            }}
+          >
+            <Image style={styles.avatar} source={{ uri: dev.avatar_url }} />
+
+            <Callout onPress={() => {}}>
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.devBio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(", ")}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
       <View style={styles.searchForm}>
         <TextInput
           style={styles.searchInput}
@@ -51,9 +100,10 @@ function Main() {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
-          onChangeText={(text) => {}}
+          value={techs}
+          onChangeText={setTechs}
         />
-        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
           <MaterialIcons name="my-location" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
@@ -64,6 +114,27 @@ function Main() {
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: "#FFF",
+  },
+  callout: {
+    width: 260,
+  },
+  devName: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  devBio: {
+    color: "#666",
+    marginTop: 5,
+  },
+  devTechs: {
+    marginTop: 5,
   },
   searchForm: {
     position: "absolute",
